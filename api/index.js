79c79fad7a9b2ee98bef7ba2d11d94ec3c0918a4
Harshith <harshith.21bce8855@vitapstudent.ac.in>
2023-11-09 22:03:00 +0530

    create and run the server

---
+++ api/index.js
@@ -0,0 +1,10 @@
+import  express  from 'express';
+// import  mongoose  from 'mongoose';
+
+// mongoose.connect("mongodb+srv://harshith:harshith@mern-estate.0aytsbm.mongodb.net/?retryWrites=true&w=majority")
+const app= express();
+
+app.listen(300, () =>{
+    console.log('Server  is running on port 3000!!');
+}
+)
