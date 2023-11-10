fca77977a5231845f7e19186c70b3b7db0bc82e0
Harshith <harshith.21bce8855@vitapstudent.ac.in>
2023-11-10 13:19:31 +0530

    complete sign up page funcionality

--- api/index.js
+++ api/index.js
@@ -23,6 +23,7 @@ app.use(express.json());
 
 app.use('/api/user',userRouter);
 app.use('/api/auth',authRouter);
+
 app.use((err,req,res,next)=>{
     const statuscode = err.statuscode || 500;
     const message = err.message || 'Internal Server Error'
