d1544db1c949e58680664066a8937877541d9dd8
Harshith <harshith.21bce8855@vitapstudent.ac.in>
2023-11-11 18:40:24 +0530

    complete update user functionality

--- api/index.js
+++ api/index.js
@@ -1,38 +1,38 @@
-import  express  from 'express';
-import  mongoose  from 'mongoose';
-import  dotenv  from 'dotenv';
-import userRouter from './routes/user.route.js';
-import authRouter from './routes/auth.route.js';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 dotenv.config();
 
-mongoose.connect(process.env.MONGO)
-.then(() =>{
-    console.log('connected to MongoDB!');
-})
-.catch((err)=>{
-    console.log('err');
-});
-const app= express();
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("connected to MongoDB!");
+  })
+  .catch((err) => {
+    console.log("err");
+  });
+const app = express();
 
 app.use(express.json());
 
-app.listen(3000, () =>{
-    console.log('Server  is running on port 3000!!');
-}
-);
+app.use(cookieParser());
 
-
-app.use('/api/user',userRouter);
-app.use('/api/auth',authRouter);
-
-app.use((err,req,res,next)=>{
-    const statuscode = err.statuscode || 500;
-    const message = err.message || 'Internal Server Error'
-    return res.status(statuscode).json({
-        success:false,
-        statuscode,
-        message,
-    });
+app.listen(3000, () => {
+  console.log("Server  is running on port 3000!!");
 });
 
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
 
+app.use((err, req, res, next) => {
+  const statuscode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statuscode).json({
+    success: false,
+    statuscode,
+    message,
+  });
+});
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -7,16 +7,21 @@ import {
 } from "firebase/storage";
 import { useRef, useState, useEffect } from "react";
 import { app } from "../firebase";
+import {
+  updateUserStart,
+  updateUserSuccess,
+  updateUserFailure,
+} from "../redux/user/userSlice";
+import { useDispatch } from "react-redux";
 export default function Profile() {
   const fileRef = useRef(null);
-  const { currentUser } = useSelector((state) => state.user);
+  const { currentUser, loading, error } = useSelector((state) => state.user);
   const [file, setFile] = useState(undefined);
   const [filePerc, setFileperce] = useState(0);
   const [fileUploadError, setFileUploadError] = useState(false);
   const [formData, setFormData] = useState({});
-  console.log(formData);
-  console.log(filePerc);
-  console.log(fileUploadError);
+  const dispatch = useDispatch();
+
   useEffect(() => {
     if (file) {
       handleFileUpload(file);
@@ -47,10 +52,39 @@ export default function Profile() {
     );
   };
 
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      dispatch(updateUserStart());
+      console.log(currentUser._id, formData);
+      const res = await fetch(`/api/user/update/${currentUser._id}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await res.json();
+      console.log(data);
+      if (data.success === false) {
+        dispatch(updateUserFailure(data.message));
+        return;
+      }
+      dispatch(updateUserSuccess(data));
+    } catch (error) {
+      dispatch(updateUserFailure(error.message));
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-bold text-center my-7">Profile</h1>
-      <form className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input
           onChange={(e) => setFile(e.target.files[0])}
           type="file"
@@ -80,28 +114,39 @@ export default function Profile() {
         <input
           type="text"
           placeholder="username"
+          defaultValue={currentUser.username}
           className="border p-3 rounded-lg"
           id="username"
+          onChange={handleChange}
         />
         <input
           type="email"
           placeholder="email"
+          defaultValue={currentUser.email}
           className="border p-3 rounded-lg"
           id="email"
+          onChange={handleChange}
         />
         <input
           type="password"
           placeholder="password"
           className="border p-3 rounded-lg"
           id="password"
+          onChange={handleChange}
         />
-        <button className="bg-green-700 text-white rounded-lg p-3 uppercase opacity-95 disabled:opacity-80">
-          update
+        <button
+          disabled={loading}
+          className="bg-green-700 text-white rounded-lg p-3 uppercase opacity-95 disabled:opacity-80"
+        >
+          {loading ? "Loading..." : "Update"}
         </button>
       </form>
       <div className="flex justify-between mt-5">
         <span className="text-red-700 cursor-pointer">Delete Account</span>
         <span className="text-red-700 cursor-pointer">Sign out</span>
+        <div>
+          <p className="text-red-700 mt-5">{error ? error : " "}</p>
+        </div>
       </div>
     </div>
   );
